b1b5e20c71c20a68a726250d8b2cd0dad818d51a
Aanbayusaputra <aanbayusaputra06@gmai.com>
2023-04-15 00:26:33 +0700

    first commit

---
+++ pages/biodata.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Biodata</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body class="bg-danger">
+    <div class=" bg-warning container">
+		<h1 class="text-center mt-5">Biodata Diri</h1>
+		<hr>
+        <h2 class="mt-3"><?php echo "Aan Bayu Saputra"; ?></h2>
+        <hr>
+
+        
+		<div  class="bg-secondary row justify-content-center">
+			<div class="col-md-4 text-center">
+				
+            <div class="card" style="width: 18rem;">
+  <img src="../assets/images/by.jpg" class="card-img-top" alt="foto">
+   
+  </div>>
+</div>
+				<h2 class="mt-3">Aan Bayu Saputra</h2>
+				<p>Brebes, 28 Juni 2002</p>
+				<p>Laki Laki</p>
+				<p>Wanasari,Brebes,Jawa Tengah</p>
+			</div>
+			<div class="col-md-6 mt-3">
+				<h3>Riwayat Pendidikan</h3>
+				<ul class="list-group">
+					<li class="list-group-item">SMA Negeri 1 Larangan</li>
+					<li class="list-group-item">Universitas Islam Sultan Agung</li>
+					
+				</ul>
+
+				<h3 class="mt-3">Hobby</h3>
+				<ul class="list-group">
+					<li class="list-group-item">Mancing</li>
+					<li class="list-group-item">Berenang</li>
+					<li class="list-group-item">Bulu Tangkis</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
